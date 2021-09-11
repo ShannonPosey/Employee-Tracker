f18cd2d53b1c9b3cde46104b20284c5d2e9a9d32
shannon_p <snp1284kasey@aol.com>
2021-09-11 13:44:53 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT into department (department_name)
+VALUES
+(); 
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+();
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+();
+
+INSERT INTO manager (first_name, last_name)
+VALUES
+();

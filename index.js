b58a48d535d39860b234667d6aaec570b59c4996
shannon_p <snp1284kasey@aol.com>
2021-09-12 16:33:33 -0400

    swtich function

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const inquirer = require("inquirer");
 const db = require("./db/connection");
+const {viewRoles, addRole} = require("./lib/roles");
 
 const promptUser = () => {
-    return inquirer
+    inquirer
     .prompt([
        {
            type: "list",
            name: "selection",
-           message: "What would you like to do?",
+           message: "What would you like to do? (Please Chose from the list below.",
            choices: ["View all departments", " View all roles", "View all employees", "Add a department", "Add a Role", "Add an employee", "Update an employee role"]
         }
     ])
-}
+    .then((data) => {
+        switch (data["selection"]) {
+            case "":
+                // function();
+                break;
+            case "":
+                // function();
+                break;
+            case "":
+                // function();
+                break;
+            case "":
+                // function();
+                break;
+            case "":
+                // function();
+                break;
+            case "":
+                // function();
+                break;
+            case "":
+                // function();
+                break;
+            case "":
+                // function();
+                break;
+            default
+        }
+    })
+};
+
+module.exports = promptUser;
+
+promptUser();

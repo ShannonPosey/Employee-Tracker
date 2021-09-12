f89069f5deb9581ec7f18f5ab1f6e1013cf3c900
shannon_p <snp1284kasey@aol.com>
2021-09-11 23:43:55 -0400

    added questions for user input

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const inquirer = require("inquirer");
 const db = require("./db/connection");
+
+const promptUser = () => {
+    return inquirer
+    .prompt([
+       {
+           type: "list",
+           name: "selection",
+           message: "What would you like to do?",
+           choices: ["View all departments", " View all roles", "View all employees", "Add a department", "Add a Role", "Add an employee", "Update an employee role"]
+        }
+    ])
+}
